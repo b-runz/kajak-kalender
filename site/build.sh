87c4fb56020dd060dc6ba0e02a04b301975336a6
b-runz <bjarke.runz@outlook.dk>
2025-04-24 14:41:10 +0200

    Migrated to astro

--- site/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p dist/about
-cp index.html dist/.
-find about -name '*.md' -exec sh -c './node_modules/.bin/markdown-it-cli {} -o dist/about/$(basename {} .md).html' \;
-exit 0
